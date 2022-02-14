650c94d6d7122bc0a560948b3dc53388666cc9c0
Andrea Ferraboli <andreaferraboli2004@gmail.com>
2022-02-14 21:41:32 +0100

    project of impiccato first version

---
+++ ESERCIZISOCKETFERRABOLI/src/impiccato/ClientTCP.java
@@ -0,0 +1,22 @@
+package impiccato;
+
+
+
+
+public class ClientTCP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ClientStr cliente = new ClientStr();
+        cliente.connetti();
+
+        while(true) {
+            cliente.comunica();
+        }
+
+        
+    }
+    
+}
---
+++ ESERCIZISOCKETFERRABOLI/src/impiccato/ServerTCP.java
@@ -0,0 +1,27 @@
+
+package impiccato;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+public class ServerTCP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+
+        int serverPort = 6789;
+        ServerSocket serverSocket = new ServerSocket(serverPort);
+
+        while(true)
+        {
+            System.out.println("Server in attesa...");
+            Socket newClient = serverSocket.accept();
+            System.out.println("Client connesso...");
+            ServerThread clientConnection = new ServerThread(newClient);
+            clientConnection.start();
+
+        }
+    }
+    
+}
